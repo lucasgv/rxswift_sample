c16d6292f07ec8dbb9d20b7f698bc97eaa34f2e6
Lucas Valle <lucasgoes@ioasys.com.br>
2017-09-28 18:05:54 -0300

    Implementa busca simples

--- rxswift_sample/rxswift_sample/ViewController.swift
+++ rxswift_sample/rxswift_sample/ViewController.swift
@@ -7,19 +7,49 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var shownCities = [String]()
+    let allCities = ["New York", "London", "Oslo", "Warsaw", "Berlin", "Praga"]
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        
+        searchBar
+            .rx.text // Observable property thanks to RxCocoa
+            .orEmpty // Make it non-optional
+            .debounce(0.5, scheduler: MainScheduler.instance) // Wait 0.5 for changes.
+            .distinctUntilChanged() // If they didn't occur, check if the new value is the same as old.
+            .filter { !$0.isEmpty }
+            .subscribe(onNext: { [unowned self] query in // Here we will be notified of every new value
+                self.shownCities = self.allCities.filter { $0.hasPrefix(query) } // We now do our "API Request" to find cities.
+                self.tableView.reloadData() // And reload table view data.
+            })
+            .addDisposableTo(disposeBag) // When you subscribe to observables, often times you want to unsubscribe from it when object is being deallocated. In Rx we have something called DisposeBag which is normally used to keep all things that you want to unsubscribe from in the deinit process
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shownCities.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cityPrototypeCell", for: indexPath)
+        cell.textLabel?.text = shownCities[indexPath.row]
+        
+        return cell
+    }
 }
 
